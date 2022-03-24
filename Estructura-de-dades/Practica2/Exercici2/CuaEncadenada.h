c8aa0a45501d7d2d3ac2d0f23459f7cecfac117a
Javiooli <jpedralo7@alumnes.ub.edu>
2022-03-24 12:28:54 +0100

    bngedwtaeSAZSEWRYU5K80

--- Estructura-de-dades/Practica2/Exercici2/CuaEncadenada.h
+++ Estructura-de-dades/Practica2/Exercici2/CuaEncadenada.h
@@ -3,21 +3,50 @@
 
 #include "Node.h"
 
-template <typename T>
+template <class T>
 class CuaEncadenada {
     public:
         CuaEncadenada();
-        CuaEncadenada(const CuaEncadenada<T>& q);
+        CuaEncadenada(const CuaEncadenada<T>& other);
         ~CuaEncadenada();
-        bool isEmpty();
-        void print();
+        bool isEmpty() const;
+        void print() const;
         void enqueue(const T key);
         void dequeue();
-        const T getFront();
+        const T getFront() const;
 
     private:
         Node<T>* _front;
         Node<T>* _rear;
 }; //CuaEncadenada
+
+template <class T>
+CuaEncadenada<T>::CuaEncadenada() {
+    Node<T> centinella = new Node<T>();
+    this->_front = *centinella;
+    this->_rear = *centinella;
+}
+
+template <class T>
+CuaEncadenada<T>::CuaEncadenada(const CuaEncadenada<T>& other) {
+    this->_front = other.getFront();
+    while (!other.isEmpty()) {
+        this->_rear->setNext(other.getFront());
+        this->_rear = other.getFront();
+        other.dequeue();
+    }
+}
+
+template <class T>
+CuaEncadenada<T>::~CuaEncadenada() {
+
+}
+
+template <class T>
+bool CuaEncadenada<T>::isEmpty() const {
+
+}
+
+
 
 #endif
--- Estructura-de-dades/Practica2/Exercici2/Node.h
+++ Estructura-de-dades/Practica2/Exercici2/Node.h
@@ -1,17 +1,58 @@
 #ifndef Node_H
 #define Node_H
 
-template <typename T>
+template <class T>
 class Node {
     public:
         Node();
-        getElement();
-        getNext();
-        setNext();
+        Node(const T& key);
+        Node(const Node<T>& other);
+        ~Node();
+        const T& getElement() const;
+        const Node<T>* getNext() const;
+        void setNext(const Node<T>* key);
 
     private:
-        T element;
-        T* next;
+        T _element;
+        Node<T>* _next;
 };
+
+
+template <class T>
+Node<T>::Node() {
+    this->_next = nullptr;
+}
+
+template <typename T>
+Node<T>::Node(const T& key) {
+    this->_element = key;
+    this->_next = nullptr;
+}
+
+template <typename T>
+Node<T>::Node(const Node<T>& other) {
+    this->_element = other.getElement();
+    this->_next = other.getNext();
+}
+
+template <typename T>
+Node<T>::~Node() {
+
+}
+
+template <typename T>
+const T& Node<T>::getElement() const {
+    return this->_element;
+}
+
+template <typename T>
+const Node<T>* Node<T>::getNext() const {
+    return this->_next;
+}
+
+template <typename T>
+void Node<T>::setNext(const Node<T>* key) {
+    this->_next = key;
+}
 
 #endif
